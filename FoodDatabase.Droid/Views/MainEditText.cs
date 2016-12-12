392cb42a48d1935646cdc9fbc93832019e56ab97
Onur Karaman <onur@areondev.de>
2016-12-12 14:05:19 +0100

    Droid: Main: Added ListView and Search editText.

---
+++ FoodDatabase.Droid/Views/MainEditText.cs
@@ -0,0 +1,41 @@
+﻿using Android.Widget;
+using Android.App;
+using Android.Content;
+using Android.Util;
+
+namespace FoodDatabase.Droid.Views
+{
+    public class MainEditText : EditText
+    {
+        private Activity _activity;
+
+        public MainEditText(Context context) : base(context)
+        {
+            InitializeView(context);
+        }
+
+        public MainEditText (Context context, IAttributeSet attrs) 
+            : base(context, attrs)
+        {
+            InitializeView(context);
+        }
+
+        public MainEditText (Context context, IAttributeSet attrs, int defStyle) 
+            : base(context, attrs, defStyle)
+        {
+            InitializeView(context);
+        }
+
+        private void InitializeView(Context context)
+        {
+            _activity = (Activity)context;
+            this.Typeface = Android.Graphics.Typeface.CreateFromAsset(Context.Assets, "fonts/segoeui.ttf");
+        }
+
+        public void ChangeFontSize(float size)
+        {
+            this.SetTextSize(ComplexUnitType.Sp, size);
+        }
+    }
+}
+
